50782020132b3b5868d836137fc01a6e3bc4e13f
Kenneth Petiote <Ken@Kens-iMac.local>
2021-07-23 18:42:28 -0400

    Heroku Link

---
+++ client/build/precache-manifest.f54b4f9f264ef871aeed104fda7d7150.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d700c17a7326df9433b",
+    "url": "/static/css/main.8f8f738b.chunk.css"
+  },
+  {
+    "revision": "1d700c17a7326df9433b",
+    "url": "/static/js/main.1008b7ad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "79bd55a4793d57ca49a8",
+    "url": "/static/css/2.78e593b9.chunk.css"
+  },
+  {
+    "revision": "79bd55a4793d57ca49a8",
+    "url": "/static/js/2.49f97fd4.chunk.js"
+  },
+  {
+    "revision": "80aba606f9770e5dc59f4a078a57762f",
+    "url": "/static/media/fixit-5-jumbotron.80aba606.png"
+  },
+  {
+    "revision": "dcfaa5b55bf5ab4d6f72b1163c8b0f7c",
+    "url": "/index.html"
+  }
+];
