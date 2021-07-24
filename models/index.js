8356f9d0064ccff1d91d16dd8375cd0e7870116a
Kenneth Petiote <Ken@Kens-iMac.local>
2021-07-23 23:34:27 -0400

    Changed start location in package.json

--- models/index.js
+++ models/index.js
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 module.exports = {
     GoogleBooks: require("./book")
 };
-=======
+
 const Job = require('./Job');
 const Customer = require('./Customer');
 const Employee = require('./Employee');
@@ -17,4 +16,4 @@ Customer.hasMany(Job, {
 });
 
 module.exports = { Job, Customer, Employee };
->>>>>>> 596ef9fc36c22ccbb95e6d070d724d0ead87b616
+
